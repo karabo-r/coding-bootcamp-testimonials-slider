07e81e27ded26a2c16eb43b6ae1c5908799378d5
karabo-r <karaborabothata0@gmail.com>
2021-10-08 12:58:02 -0700

    new logic update with json

--- main.js
+++ main.js
@@ -1,19 +1,47 @@
-let profile_name = document.getElementById("name");
-let profile_comment = document.getElementById("comment");
-let profile_job_title = document.getElementById("job-title");
-let btn = document.getElementById("btn");
+const profiles = [
+  {
+    id: 1,
+    name: "Tanya Sinclair",
+    job: "UX Engineer",
+    image: '"./images/image-tanya.jpg"',
+    comment:
+      "I’ve been interested in coding for a while but never taken the jump, until\r\n    now. I couldn’t recommend this course enough. I’m now in the job of my\r\n    dreams and so excited about the future.",
+  },
+  {
+    id: 2,
+    name: "John Tarkpor",
+    job: " Junior Front-end Developer",
+    image: "/images/image-john.jpg",
+    comment:
+      "If\r\n    you want to lay the best foundation possible I’d recommend taking this\r\n    course. The depth the instructors go into is incredible. I now feel so\r\n    confident about starting up as a professional developer.",
+  },
+];
 
-let request = new Request("./profiles.json");
+const profileComment = document.getElementById("comment");
+const profileName = document.getElementById("name");
+const profileJobTitle = document.getElementById("job-title");
+const profileImage = document.getElementById("profile-image");
 let trackIdNumber = 0;
 
-fetch(request).then((resp) =>
-  resp.json().then((data) => {
-    appendData(data);
-  })
-);
-
-const appendData = (data) => {
-  profile_name.innerHTML = data[trackIdNumber].name;
-  profile_job_title.innerHTML = data[trackIdNumber].job_title;
-  profile_comment.innerHTML = data[trackIdNumber].comment;
+const next = () => {
+  trackIdNumber = 1;
+  updateDom();
 };
+
+const prev = () => {
+  trackIdNumber = 0;
+  updateDom();
+};
+
+if (trackIdNumber === 0) {
+  updateDom();
+}
+
+function updateDom() {
+  profileComment.textContent = profiles[trackIdNumber].comment;
+  profileName.textContent = profiles[trackIdNumber].name;
+  profileJobTitle.textContent = profiles[trackIdNumber].job;
+  profileImage.style = `background-image:url(${profiles[trackIdNumber].image})`;
+}
+
+console.log(profiles[trackIdNumber].image);
