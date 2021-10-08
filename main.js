d91d025b507482741524a7902e259f839dcb5886
karabo-r <karaborabothata0@gmail.com>
2021-10-08 13:36:49 -0700

    update before mobile version

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Tried storing the profile data in a json file but got complicated. Profiles object looks messy thou ,sorry 💔
 const profiles = [
   {
     id: 1,
@@ -5,7 +6,7 @@ const profiles = [
     job: "UX Engineer",
     image: '"./images/image-tanya.jpg"',
     comment:
-      "I’ve been interested in coding for a while but never taken the jump, until\r\n    now. I couldn’t recommend this course enough. I’m now in the job of my\r\n    dreams and so excited about the future.",
+      " I’ve been interested in coding for a while but never taken the jump, until\r\n    now. I couldn’t recommend this course enough. I’m now in the job of my\r\n    dreams and so excited about the future. ",
   },
   {
     id: 2,
@@ -13,7 +14,7 @@ const profiles = [
     job: " Junior Front-end Developer",
     image: "/images/image-john.jpg",
     comment:
-      "If\r\n    you want to lay the best foundation possible I’d recommend taking this\r\n    course. The depth the instructors go into is incredible. I now feel so\r\n    confident about starting up as a professional developer.",
+      " If\r\n    you want to lay the best foundation possible I’d recommend taking this\r\n    course. The depth the instructors go into is incredible. I now feel so\r\n    confident about starting up as a professional developer. ",
   },
 ];
 
@@ -21,8 +22,18 @@ const profileComment = document.getElementById("comment");
 const profileName = document.getElementById("name");
 const profileJobTitle = document.getElementById("job-title");
 const profileImage = document.getElementById("profile-image");
+
+// State
 let trackIdNumber = 0;
 
+const updateDom = () => {
+  profileImage.style = `background-image:url(${profiles[trackIdNumber].image})`;
+  profileComment.innerHTML = `<p> &ldquo; ${profiles[trackIdNumber].comment}&rdquo; </p>`;
+  profileName.innerHTML = profiles[trackIdNumber].name;
+  profileJobTitle.innerHTML = profiles[trackIdNumber].job;
+};
+
+// Button event functions
 const next = () => {
   trackIdNumber = 1;
   updateDom();
@@ -33,15 +44,4 @@ const prev = () => {
   updateDom();
 };
 
-if (trackIdNumber === 0) {
-  updateDom();
-}
-
-function updateDom() {
-  profileComment.textContent = profiles[trackIdNumber].comment;
-  profileName.textContent = profiles[trackIdNumber].name;
-  profileJobTitle.textContent = profiles[trackIdNumber].job;
-  profileImage.style = `background-image:url(${profiles[trackIdNumber].image})`;
-}
-
-console.log(profiles[trackIdNumber].image);
+updateDom();
